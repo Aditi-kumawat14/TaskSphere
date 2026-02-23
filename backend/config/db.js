ffbd60913819a46a50c16970ad6302b2a54b20b7
Aditi-kumawat14 <163274755+Aditi-kumawat14@users.noreply.github.com>
2026-02-23 13:08:02 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,13 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,   // IMPORTANT
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect((err) => {
@@ -12,7 +16,7 @@ db.connect((err) => {
         console.error("Database connection failed:", err);
         return;
     }
-    console.log("MySQL Connected");
+    console.log("Connected to Aiven MySQL ✅");
 });
 
 module.exports = db;
